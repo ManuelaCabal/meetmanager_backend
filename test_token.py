814519cd6b4c9040091042b47cd4d76fd97e56ff
ManuelaCabal <cabal.saman25@zaragoza.salesianos.edu>
2026-01-25 02:47:23 +0100

    Limpio test token

--- test_token.py
+++
@@ -1,9 +0,0 @@
-# test_token.py
-import requests
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-TOKEN = os.getenv("TELEGRAM_TOKEN")
-r = requests.get(f"https://api.telegram.org/bot{TOKEN}/getMe")
-print(r.json())
